7b326ef414da898ca6c997a6d810290187627da1
lxjwatch <1928615906@qq.com>
2024-06-10 09:53:15 +0800

    工厂+策略 新增题目

---
+++ xy-club-subject/xy-club-domain/target/generated-sources/annotations/com/xxxy/subject/domain/convert/BriefSubjectConverterImpl.java
@@ -0,0 +1,27 @@
+package com.xxxy.subject.domain.convert;
+
+import com.xxxy.subject.domain.entity.SubjectInfoBO;
+import com.xxxy.subject.infra.basic.entity.SubjectBrief;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-06-10T09:12:19+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_362 (Amazon.com Inc.)"
+)
+public class BriefSubjectConverterImpl implements BriefSubjectConverter {
+
+    @Override
+    public SubjectBrief convertBoToEntity(SubjectInfoBO subjectInfoBO) {
+        if ( subjectInfoBO == null ) {
+            return null;
+        }
+
+        SubjectBrief subjectBrief = new SubjectBrief();
+
+        subjectBrief.setId( subjectInfoBO.getId() );
+        subjectBrief.setSubjectAnswer( subjectInfoBO.getSubjectAnswer() );
+
+        return subjectBrief;
+    }
+}
